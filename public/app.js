b86c8da5577f06c5ab43280e8edbb525d803d07a
Matt <mspur054@uottawa.ca[core]>
2017-09-19 21:18:20 -0400

    Added post deletion.

--- public/app.js
+++ public/app.js
@@ -6,10 +6,57 @@
 		function BlogController($scope, $http){
 			$scope.createPost = createPost;
 
+			$scope.deletePost = deletePost;
+			$scope.editPost = editPost;
+
+
+			function init(){
+				getAllPosts();
+			}
+			init();
+
+
+			function editPost(postID){
+				$http
+					.get("/api/blogpost/" + postID)
+					.then(function(post){
+						$scope.post = post.data;
+					}, function(error){
+						console.log(error);
+					});
+			}
+
+
+			function deletePost(postID){
+				$http
+					.delete("/api/blogpost/" + postID)
+					.then(function(){
+						getAllPosts();
+					},function(error){
+						console.log(error);
+					});
+			}
+
+
+			function getAllPosts(){
+				$http
+					.get("/api/blogpost")
+					.then(function(posts){
+						$scope.posts = posts.data;
+					}, function(error){
+						console.log(error);
+					});
+			}
+
 			function createPost(post) {
-				//client side
-				console.log(post);
-				$http.post("/api/blogpost", post);
+				$http
+					.post("/api/blogpost", post)
+					.then(function(){
+						getAllPosts();
+					}, function(error){
+						console.log(error);
+					});
+
 			}
 		}
 })();
--- server.js
+++ server.js
@@ -5,25 +5,86 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blog2017', { useMongoClient: true, promiseLibrary: global.Promise });
 
-const postSchema = mongoose.Schema({
+const PostSchema = mongoose.Schema({
 	title: {type:String, required: true},
 	body: String,
+	tag:{type:String, enum: ['Technology', 'Learning','Economy','Javascript']},
 	posted: {type:Date, default: Date.now}
-});
+}, {collection: 'post'});
+
+const PostModel = mongoose.model("PostModel", PostSchema);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); //for parsing json
 app.use(bodyParser.urlencoded({extended:true})); //for parsing applications
 
 app.post("/api/blogpost", createPost);
+app.get("/api/blogpost", getAllPosts);
+app.delete("/api/blogpost/:id", deletePost);
+app.get("/api/blogpost/:id", getPostByID);
+
+
+function getPostByID(req, res){
+	const postID = req.params.id;
+
+	PostModel
+		.findById(postID)
+		.then(
+			function(post){
+				res.json(post);
+		},
+		function(error){
+			res.sendStatus(400);
+		});
+}
+
+
+function deletePost(req, res){
+	//delete a post, supply an id
+	const postID = req.params.id;
+
+	PostModel
+		.remove({_id: postID})
+		.then(function(status){
+			res.sendStatus(200);
+		},function(){
+			res.sendStatus(400);
+		});
+}
+
+function getAllPosts(req, res){
+	PostModel
+		.find()
+		.then(
+			function(posts){
+				res.json(posts);
+			},
+			function(error){
+				res.sendStatus(400);
+			});
+}
 
 function createPost(req, res){
 //creates post on server
 	const post = req.body;
+	
+	PostModel
+		.create(post)
+		.then( //promises
+			function(postObj){
+				res.json(200);
+			},
+			function(error){
+				res.sendStatus(400);
+			}
+		); //insert into db
 	console.log(post);
 	res.json(post);
+
 }
+
+
+
  
 app.listen(3000);
-
 
